7206642f9b3d12e65ae1278e554b0bb3bb20522c
OrlinGit <orlin.nikolov@hotmail.com>
2025-04-30 19:09:59 +0300

    Task 12 is toghether with Task.java

---
+++ 07 Stack and Queue/Task.java
@@ -0,0 +1,10 @@
+public class Task {
+	public String task;
+	public int priority;
+
+	public Task (String task, int priority){
+		this.task = task;
+		this.priority = priority;
+	}
+
+}
--- 07 Stack and Queue/Task12_TaskScheduler.java
+++ 07 Stack and Queue/Task12_TaskScheduler.java
@@ -1,2 +1,31 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
 public class Task12_TaskScheduler {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter task: ");
+		String input = sc.nextLine();
+		PriorityQueue<Task> taskQueue = new PriorityQueue<>(
+				(a,b) -> {
+					if (a.priority == b.priority){
+						return  0;
+					}
+					if (a.priority > b.priority){
+						return  -1;
+					} else {
+						return  1;
+					}
+				}
+		);
+		while(!input.equals("getNextTask")){
+			String[] commands = input.split(" ");
+			taskQueue.add(new Task(commands[1], Integer.parseInt(commands[2])));
+			System.out.println("Enter task: ");
+			input = sc.nextLine();
+		}
+
+		Task taskHighPriority = taskQueue.poll();
+		System.out.println(taskHighPriority.task + " - " + taskHighPriority.priority);
+	}
 }
